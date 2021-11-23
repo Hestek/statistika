a363a2e63d5a7f6ea6db37564b3ed4d4e3cfc40a
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2021-11-23 11:02:50 +0100

    k

--- statistika/Program.cs
+++ statistika/Program.cs
@@ -12,7 +12,15 @@ namespace statistika
             do
             {
                 keyInfo = Console.ReadKey(true);
-                if (povoleneznaky.Contains(keyInfo.KeyChar));
+                if (povoleneznaky.Contains(keyInfo.KeyChar))
+                {
+                    //zde se program dostane při stisku čísla
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                {
+
+                }
+                else if (keyInfo.Key == ConsoleKey.Backspace)
                 {
 
                 }
